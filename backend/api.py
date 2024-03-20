febb878ce502fa6203ca1ed01a81b212a3885a86
Szymon Dębski <szymon.debski@vee.ai>
2024-03-20 15:31:40 +0100

    authorization added

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,12 @@
 from ninja import NinjaAPI
+from core.auth import ApiKeyAuth, KnoxAuth
 from company.api import router as company_router
 from devices.api import router as devices_router
+from core.api import router as core_router
 
 
-api = NinjaAPI()
+api = NinjaAPI(auth=[ApiKeyAuth(), KnoxAuth()])
 
+api.add_router("auth/", core_router)
 api.add_router("company/", company_router)
 api.add_router("devices-api/", devices_router)
