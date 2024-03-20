32315995e87c4b55eb3e3af19212d7eaf2ec2c21
Szymon Dębski <szymon.debski@vee.ai>
2024-03-20 16:01:48 +0100

    projects schema and api added

---
+++ company/api/project.py
@@ -0,0 +1,60 @@
+from ninja import Router
+from http import HTTPStatus
+from django.shortcuts import get_object_or_404
+
+from company.schemas import ProjectReadSchema, ProjectWriteSchema
+from company import models
+
+router = Router(tags=["Projects"])
+
+
+@router.get("", response={HTTPStatus.OK: list[ProjectReadSchema]})
+def get_projcts(request):
+    return HTTPStatus.OK, models.Project.objects.all()
+
+
+@router.get("{id}/", response={HTTPStatus.OK: ProjectReadSchema})
+def get_project(request, id: int):
+    project = get_object_or_404(models.Project, id=id)
+    return HTTPStatus.OK, project
+
+
+@router.post(
+    "",
+    response={
+        HTTPStatus.CREATED: ProjectReadSchema,
+        HTTPStatus.BAD_REQUEST: None,
+    },
+)
+def create_project(request, project_data: ProjectWriteSchema):
+    project_data = project_data.model_dump()
+
+    return HTTPStatus.CREATED, models.Project.objects(**project_data)
+
+
+@router.api_operation(
+    ["PUT", "PATCH"],
+    "{id}/",
+    response={
+        HTTPStatus.OK: ProjectReadSchema,
+        HTTPStatus.BAD_REQUEST: None,
+    },
+)
+def update_project(request, id: int, project_data: ProjectWriteSchema):
+    project = get_object_or_404(models.Project, id=id)
+
+    project_data = project_data.dict()
+    for attr, value in project_data.items():
+        if value:
+            setattr(project, attr, value)
+    project.save()
+
+    return HTTPStatus.OK, project
+
+
+@router.delete("{id}/", response={HTTPStatus.NO_CONTENT: None})
+def delete_project(request, id: int):
+    project = get_object_or_404(models.Project, id=id)
+    project.delete()
+
+    return HTTPStatus.NO_CONTENT, None
